ce3874a179d66cab077fdadbfc0f17a90b7da10e
Luka-dot <79lukask@gmail.com>
2020-12-22 19:53:33 -0500

    caseStudfies added

--- src/components/cases.jsx
+++ src/components/cases.jsx
@@ -2,6 +2,26 @@ import React from 'react';
 import {ReactComponent as CasesNext } from '../assets/right-arrow.svg';
 import {ReactComponent as CasesPrev } from '../assets/left-arrow.svg';
 
+const caseStudies = [
+    {id: 1, 
+    subtitle: 'Subtitle One', 
+    title: 'Title for project One',
+    img: 'first.jpg'
+    },
+    {
+        id: 2, 
+        subtitle: 'Subtitle TWO', 
+        title: 'Title for project TWO',
+        img: 'second.jpg'
+    },    
+    {
+        id: 3, 
+        subtitle: 'Subtitle Three', 
+        title: 'Title for project 3',
+        img: 'third.jpg'
+    }
+];
+
 const Cases = () => {
 
     return (
@@ -14,6 +34,9 @@ const Cases = () => {
                     <div className="cases-arrow next">
                         <CasesNext />
                     </div>
+                </div>
+                <div className="row">
+                    <div className="case"></div>
                 </div>
             </div>
         </section>
