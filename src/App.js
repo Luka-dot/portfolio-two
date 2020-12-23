0b7df07b1b25601ff41052c709c8461b2058fe0c
Luka-dot <79lukask@gmail.com>
2020-12-23 08:39:13 -0500

    code splitted and HomePage set up

--- src/App.js
+++ src/App.js
@@ -2,65 +2,20 @@ import React, {useEffect} from 'react';
 import gsap from 'gsap';
 import './styles/App.scss';
 import Header from './components/header';
-import Banner from './components/banner';
-import Cases from './components/cases';
-import IntroOverlay from './components/introOverlay';
+import Home from './pages/home';
 
 function App() {
 
-  // this useEffect is to calculate vh in banner.scss
   useEffect(() => {
-    let vh =window.innerHeight * .01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-
     // line below prevent "flashing" on refresh
     gsap.to("body", 0, { css: {visibility: "visible"}});
-    //timeline
-    const tl = gsap.timeline();
-
-    // tl.from(".main h2", 2, {
-    //   opacity: 0,
-    //   y: 70
-    // });
-
-    tl.from(".line span", 1.8, {
-      y: 100,
-      ease: "power4.out",
-      delay: 1,
-      skewY: 7,
-      stagger: {
-        amount: 0.3
-      }
-    }).to(".overlay-top", 1.6, {
-      height: 0,
-      ease: "expo.inOut",
-      stagger: 0.4
-    }).to(".overlay-bottom", 1.6, {
-      width: 0,
-      ease: "expo.inOut",
-      delay: -.8,
-      stagger: {
-        amount: 0.8
-      }                                                    // intro-overlay is just laying on top so it needs to be hidde.
-    }).to(".intro-overlay", 0, { css: {display: "none"}})  // this is to hide intro-overlay so aplication is clickable
-    .from(".case-image img", 2, {
-      scale: 1.4,
-      ease: "expo.inOut",
-      delay: -2,
-      stagger: {
-        amount: 0.8
-      }
-    })
-
-  }, [])
+  })
 
   return (
-    <div className="App">
-      <IntroOverlay />
+    <>
       <Header /> 
-      <Banner />
-      <Cases />
-    </div>
+      <Home />  
+    </>
   );
 }
 
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,62 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import gsap from 'gsap';
+import Banner from '../components/banner';
+import Cases from '../components/cases';
+import IntroOverlay from '../components/introOverlay';
 
 const HomePage = () => {
 
+  useEffect(() => {
+    // calculate vh in banner.scss
+    let vh =window.innerHeight * .01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+    
+    //timeline
+    const tl = gsap.timeline();
+
+    // tl.from(".main h2", 2, {
+    //   opacity: 0,
+    //   y: 70
+    // });
+
+    tl.from(".line span", 1.8, {
+      y: 100,
+      ease: "power4.out",
+      delay: 1,
+      skewY: 7,
+      stagger: {
+        amount: 0.3
+      }
+    }).to(".overlay-top", 1.6, {
+      height: 0,
+      ease: "expo.inOut",
+      stagger: 0.4
+    }).to(".overlay-bottom", 1.6, {
+      width: 0,
+      ease: "expo.inOut",
+      delay: -.8,
+      stagger: {
+        amount: 0.8
+      }                                                    // intro-overlay is just laying on top so it needs to be hidde.
+    }).to(".intro-overlay", 0, { css: {display: "none"}})  // this is to hide intro-overlay so aplication is clickable
+    .from(".case-image img", 2, {
+      scale: 1.4,
+      ease: "expo.inOut",
+      delay: -2,
+      stagger: {
+        amount: 0.8
+      }
+    })
+
+  }, [])
+
     return (
-        <div>
-        </div>
+        <>
+            <IntroOverlay />
+            <Banner />
+            <Cases />
+        </>
     )
 };
 
