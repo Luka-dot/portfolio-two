e25bc6510e73e209b9f3c6c76f7f15200ee2fcb8
Luka-dot <79lukask@gmail.com>
2020-12-23 09:17:29 -0500

    routing added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, {useEffect} from 'react';
 import gsap from 'gsap';
+import {Route} from 'react-router-dom';
+
 import './styles/App.scss';
 import Header from './components/header';
-import Home from './pages/home';
+
+import About from './pages/about';
+import Approach from './pages/aproach';
+import CaseStudies from './pages/caseStudies';
+import Services from './pages/services';
+import HomePage from './pages/home';
+
+// routes
+const routes = [
+  { path: "/", name: "Home", Component: HomePage },
+  { path: "/case-studies", name: "CaseStudies", Component: CaseStudies },
+  { path: "/about", name: "About", Component: About },
+  { path: "/approach", name: "Approach", Component: Approach },
+  { path: "/services", name: "Services", Component: Services }
+];
 
 function App() {
 
   useEffect(() => {
+    // calculate vh in banner.scss
+    let vh =window.innerHeight * .01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
     // line below prevent "flashing" on refresh
     gsap.to("body", 0, { css: {visibility: "visible"}});
   })
@@ -14,7 +34,13 @@ function App() {
   return (
     <>
       <Header /> 
-      <Home />  
+      <div className="App">
+        {routes.map(({path, Component}) => (
+          <Route key={path} exact path={path}>
+            <Component />
+          </Route>
+        ))}
+      </div>
     </>
   );
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,59 +1,58 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import gsap from 'gsap';
 import Banner from '../components/banner';
 import Cases from '../components/cases';
 import IntroOverlay from '../components/introOverlay';
 
+const tl = gsap.timeline();
+
+const homePageAnimation = (completeAnimation) => {
+    tl.from(".line span", 1.8, {
+        y: 100,
+        ease: "power4.out",
+        delay: 1,
+        skewY: 7,
+        stagger: {
+          amount: 0.3
+        }
+      }).to(".overlay-top", 1.6, {
+        height: 0,
+        ease: "expo.inOut",
+        stagger: 0.4
+      }).to(".overlay-bottom", 1.6, {
+        width: 0,
+        ease: "expo.inOut",
+        delay: -.8,
+        stagger: {
+          amount: 0.8
+        }                                                    // intro-overlay is just laying on top so it needs to be hidde.
+      }).to(".intro-overlay", 0, { css: {display: "none"}})  // this is to hide intro-overlay so aplication is clickable
+      .from(".case-image img", 2, {
+        scale: 1.4,
+        ease: "expo.inOut",
+        delay: -2,
+        stagger: {
+          amount: 0.8
+        },
+        onComplete: completeAnimation
+    })
+}
+
 const HomePage = () => {
+    const [animationComplete, setAnimationComplete] = useState(false);
+
+    const completeAnimation = () => {
+        setAnimationComplete(true);
+    };
 
   useEffect(() => {
-    // calculate vh in banner.scss
-    let vh =window.innerHeight * .01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-    
-    //timeline
-    const tl = gsap.timeline();
-
-    // tl.from(".main h2", 2, {
-    //   opacity: 0,
-    //   y: 70
-    // });
-
-    tl.from(".line span", 1.8, {
-      y: 100,
-      ease: "power4.out",
-      delay: 1,
-      skewY: 7,
-      stagger: {
-        amount: 0.3
-      }
-    }).to(".overlay-top", 1.6, {
-      height: 0,
-      ease: "expo.inOut",
-      stagger: 0.4
-    }).to(".overlay-bottom", 1.6, {
-      width: 0,
-      ease: "expo.inOut",
-      delay: -.8,
-      stagger: {
-        amount: 0.8
-      }                                                    // intro-overlay is just laying on top so it needs to be hidde.
-    }).to(".intro-overlay", 0, { css: {display: "none"}})  // this is to hide intro-overlay so aplication is clickable
-    .from(".case-image img", 2, {
-      scale: 1.4,
-      ease: "expo.inOut",
-      delay: -2,
-      stagger: {
-        amount: 0.8
-      }
-    })
-
+    // gsap on complete
+    homePageAnimation(completeAnimation);
   }, [])
 
     return (
         <>
-            <IntroOverlay />
+            {animationComplete === false ? <IntroOverlay /> : "" }
             <Banner />
             <Cases />
         </>
