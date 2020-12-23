f7a5ffaddf707aecf5642d2a6bb240da208ca37d
Luka-dot <79lukask@gmail.com>
2020-12-23 08:19:30 -0500

    basic pages setUp

---
+++ src/pages/caseStudies.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const CaseStudies = () => {
+
+    return (
+        <div className="page">
+            <div className="container">
+                <div className="row">
+                    <h3>This is STUDIES page</h3>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default CaseStudies;
