8313d40b6968834f1a87e4c79c1e4bae2f46f0c7
Luka-dot <79lukask@gmail.com>
2020-12-23 22:12:30 -0500

    routing navigation component created

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -13,27 +13,55 @@ const Navigation = () => {
                             <NavLink to="/case-studies" exact >
                                 Case Studies
                             </NavLink>
-
                         </li>
                         <li>
                             <NavLink to="/approach" exact >
                                 Aproach
-                            </NavLink>
-                            
+                            </NavLink>                         
                         </li>
                         <li>
                             <NavLink to="/services" exact >
                                 Services
                             </NavLink>
-                            
                         </li>
                         <li>
                             <NavLink to="/about" exact >
                                 About us
                             </NavLink>
-                            
                         </li>
                     </ul>
+                </div>
+                <div className="nav-column">
+                    <div className="nav-label">Contact</div>
+                    <div className="nav-infos">
+                        <ul className="nav-info">
+                            <li className="nav-info-label">Email</li>
+                            <li>
+                                <NavLink to="/contact" exact >
+                                    Get in touch with us.
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/audit" exact >
+                                    Audit Time?
+                                </NavLink>
+                            </li>
+                        </ul>
+                        <ul className="nav-info">
+                            <li className="nav-info-label">Headquoters</li>
+                            <li>This will be the adress.</li>
+                            <li>12345 City Name</li>
+                            <li>Country</li>
+                        </ul>
+                        <ul className="nav-info">
+                            <li className="nav-info-label">Phone</li>
+                            <li>+11 123-123-1234</li>
+                        </ul>
+                        <ul className="nav-info">
+                            <li className="nav-info-label">Legal</li>
+                            <li>Privacy and cookies</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </nav>
